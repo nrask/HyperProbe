c679aea5bdf24545ce7db24d3c02014113995257
Niels <niels.rask@gmail.com>
2024-11-20 20:18:55 +0100

    Show headers, allow changing user-agent

--- HttpTest/Form1.Designer.cs
+++ HttpTest/Form1.Designer.cs
@@ -30,11 +30,12 @@
         {
             button1 = new Button();
             treeView1 = new TreeView();
+            textBox1 = new TextBox();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(354, 86);
+            button1.Location = new Point(525, 33);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 0;
@@ -44,26 +45,38 @@
             // 
             // treeView1
             // 
-            treeView1.Location = new Point(426, 205);
+            treeView1.Location = new Point(431, 77);
             treeView1.Name = "treeView1";
-            treeView1.Size = new Size(263, 190);
+            treeView1.Size = new Size(321, 361);
             treeView1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(25, 21);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(238, 122);
+            textBox1.TabIndex = 2;
+            textBox1.Text = "http://ektaliving.com/\r\nhttp://itide.dk\r\nhttps://httpstatus.io/\r\nhttp://reddit.com";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
             Controls.Add(treeView1);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
         private TreeView treeView1;
+        private TextBox textBox1;
     }
 }
--- HttpTest/Form1.cs
+++ HttpTest/Form1.cs
@@ -11,23 +11,42 @@ namespace HttpTest
 
         private void button1_Click(object sender, EventArgs e)
         {
-            QueryUrl("http://ektaliving.com/");
+            // todo: "canonical name check" funktionalitet
+            // todo: hvad nu hvis den timer ud. lav bedre fejl håndtering
 
-            var n1 = treeView1.Nodes.Add("key", "Text");
-            n1.Nodes.Add("sub1");
-            n1.Nodes.Add("sub2", "text2");
-            var n2 = treeView1.Nodes.Add("key2", "Text2");
+            treeView1.Nodes.Clear();
+            foreach (var _url in textBox1.Lines)
+            {
+                var url = _url;
+                if (!url.StartsWith("http"))
+                {
+                    url = "http://" + url;
+                }
+                //string url = "http://ektaliving.com/";
+                var node = treeView1.Nodes.Add("url", url);
+                QueryUrl(url, node);
+                node.Expand();
+            }
+
+            //var n1 = treeView1.Nodes.Add("key", "Text");
+            //n1.Nodes.Add("sub1");
+            //n1.Nodes.Add("sub2", "text2");
+            //var n2 = treeView1.Nodes.Add("key2", "Text2");
         }
 
-        private void test() { 
+        private void test()
+        {
             var handler = new HttpClientHandler()
             {
                 AllowAutoRedirect = false  // Disables automatic redirect following
+         
             };
 
             HttpClient client = new HttpClient(handler);
 
             var req = new HttpRequestMessage(HttpMethod.Get, "http://ektaliving.com/");
+            //req.Headers.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:132.0) Gecko/20100101 Firefox/132.0";
+            req.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:132.0) Gecko/20100101 Firefox/132.0");
             var sw = Stopwatch.StartNew();
             var response = client.Send(req);
             sw.Stop();
@@ -47,7 +66,7 @@ namespace HttpTest
             }
         }
 
-        private void QueryUrl(string url)
+        private void QueryUrl(string url, TreeNode node)
         {
             Debug.WriteLine($"Querying url: '{url}' ...");
             var handler = new HttpClientHandler()
@@ -57,24 +76,39 @@ namespace HttpTest
 
             HttpClient client = new HttpClient(handler);
 
+            var x = node.Nodes.Add($"Url: {url}");
+
             var req = new HttpRequestMessage(HttpMethod.Get, url);
+            req.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:132.0) Gecko/20100101 Firefox/132.0");// reddit.com giver 403 hvis vi ikke ændrer ua fra default
             var sw = Stopwatch.StartNew();
             var response = client.Send(req);
             sw.Stop();
+            x.Text += $" -> ({(int)response.StatusCode})";
 
-            Debug.WriteLine($"Got response: {(int)response.StatusCode} {response.StatusCode} - took {sw.ElapsedMilliseconds} ms, returned {response.Content.Headers.ContentLength/1024} kb");
+            x.Nodes.Add($"Response code: {(int)response.StatusCode} {response.StatusCode}");
+
+            Debug.WriteLine($"Got response: {(int)response.StatusCode} {response.StatusCode} - took {sw.ElapsedMilliseconds} ms, returned {response.Content.Headers.ContentLength / 1024} kb");
             if ((int)response.StatusCode >= 300 && (int)response.StatusCode < 400)
             {
                 // Step 5: Get the Location header to find the redirect URL
                 if (response.Headers.Location != null)
                 {
+                    x.Nodes.Add($"Destination: {response.Headers.Location}"); 
                     Debug.WriteLine($"Redirect to: {response.Headers.Location}");
-                    QueryUrl(response.Headers.Location.ToString());
+                    //NOTE: reddit.com kan finde på at give os en relativ location. det skal vi kunne håndtere
+                    QueryUrl(response.Headers.Location.ToString(), node);
                 }
                 else
                 {
                     Debug.WriteLine("Redirect without a Location header.");
                 }
+            }
+            x.Nodes.Add($"Time taken: {sw.ElapsedMilliseconds} ms");
+            x.Nodes.Add($"Response length: {response.Content.Headers.ContentLength / 1024} kb");
+            var h = x.Nodes.Add("Headers");
+            foreach (var item in response.Headers)
+            {
+                h.Nodes.Add($"{item.Key}: {string.Join(",", item.Value.Select(x => $"'{x}'"))}");
             }
 
         }
